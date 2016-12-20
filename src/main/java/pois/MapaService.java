5610f8197482c5fb2f2b1bff110ba286226ddb37
lucasdcampaner <lucasdcampaner@gmail.com>
2016-12-20 17:12:17 -0300

    Agrega mas pois al mapa de cafeterias.

--- src/main/java/pois/MapaService.java
+++ src/main/java/pois/MapaService.java
@@ -9,14 +9,17 @@ public class MapaService {
 	private Map<String,Mapa> mapas = new HashMap<String, Mapa>();
 	
 	public MapaService(){
+
 		Mapa mapaCafeterias = new Mapa("cafeterias");
 		mapaCafeterias.addPOI(new POI("Cafe los Angelitos", -34.6096435,-58.3983699));
+		mapaCafeterias.addPOI(new POI("MC Donalds", -34.6089664,-58.3973174));
+		mapaCafeterias.addPOI(new POI("Subway", -34.6095274,-58.3944184));
+		
 		this.mapas.put(mapaCafeterias.getNombre(),mapaCafeterias );
 	}
 	
-	
-
 	public List<POI> getListaDePOIS(String nombreMapa) {
+		
 		return this.mapas.get(nombreMapa).getLista();
 	}
 
